b013958b2cdb9ad7d5119ee4a61d449fa5ed89af
David-Salami <salamiidowudavid@gmail.com>
2022-04-07 21:47:21 -0700

    my C task commits

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include < stdio.n>
+
+/**
+* main . Entry point
+ * main . Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

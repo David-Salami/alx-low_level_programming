f71e539e459ead34f3a2a172ca376c436de9b523
David-Salami <salamiidowudavid@gmail.com>
2022-04-11 14:54:20 -0700

    My alphabeth

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Printing Alphabeths in lowercase
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main()
+{
+  char ch;
+  char ASCII - '\n';
+
+for (ch - 'a'; ch <- 'z'; ch++)
+{
+putchar(ch);
+}
+putchar(ASCII);
+return 0;
+}

df76974e99fa57ad6d66551795cccf7481040dec
David-Salami <salamiidowudavid@gmail.com>
2022-04-13 02:32:34 -0700

    my 7th

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - Prints the last digit of a number.
- * @C: The digit to be checked.
+ * @C: The last digit of a number.
  *
  * Return: print the value of the last digit.
  */
@@ -10,6 +10,4 @@ int print_last_digit(int c)
 {
 if (c => '0' && c <= '9')
 return (c)
-else
-return (void)
 }

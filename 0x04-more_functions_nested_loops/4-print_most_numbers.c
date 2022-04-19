dcc2aed4191c97d0f801001253cb9ac76f8c31f0
David-Salami Name <salamiidowudavid@gmail.com>
2022-04-19 06:54:03 -0700

    my 4th commit statement

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,27 +1,22 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints except 2 and 4.
+ * print_most_numbers - prints numbers except 2 and 4.
  *
  * Return: Always 0 (Success)
  */
-
 void print_most_number(void)
 {
 int i;
 
 i = 0;
 
-for (i = 0; i < 10; i++)
+for (i = 0; i <= 9; i++)
 {
-
-if (i == 2 || i == 4)
-
-i++;
-
+if ((i == 2) || (i == 4))
+continue;
+else
 _putchar(i + '0');
-
 }
-_putchar ('\n');
-
+_putchar('\n');
 }

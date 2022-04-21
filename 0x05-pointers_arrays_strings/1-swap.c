84066ded66b18aef138915df4e5c2e397ee8158e
David-Salami Name <salamiidowudavid@gmail.com>
2022-04-20 19:04:02 -0700

    My 1st commit statement

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * swap_int - swap values of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: always 0
+ */
+void swap_int(int *a, int *b)
+{
+int x;
+
+x = *a;
+*a = *b;
+*b = x;
+return;
+
+}
+
+  

cd681160f3491a8b4011ab2b4bd47b0be3547f3c
David-Salami Name <salamiidowudavid@gmail.com>
2022-04-20 21:33:35 -0700

    My 2nd commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - lenght of string
+ * @s: char
+ * Retun: int
+ */
+int _strlen(char *s)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+continue;
+}
+return (i);
+}

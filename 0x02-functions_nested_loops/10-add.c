4a4787ebe1eb9ed30300880f45f8b6f6ab156561
David-Salami Name <salamiidowudavid@gmail.com>
2022-06-10 00:52:57 -0700

    my commit statement

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function that adds two int and return result
+ * @a: first inputed variable
+ * @b: second inputed variable
+ * return: zero
+ */
+
+int add(int a, int b)
+{
+return(a + b);
+}

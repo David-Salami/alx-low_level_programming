a9ea2633236aacc8332b1f9be69dbc75473c4614
David-Salami <salamiidowudavid@gmail.com>
2022-07-29 21:42:22 -0700

    My new commit

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -7,12 +7,12 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hsh;
-	int c;
+unsigned long int hsh;
+int c;
 
-	hsh = 5381;
-	while ((c = *str++))
-		hsh = ((hsh << 5) + hsh) + c;  /* hsh * 33 + c */
+hsh = 5381;
+while ((c = *str++))
+hsh = ((hsh << 5) + hsh) + c;  /* hsh * 33 + c */
 
-	return (hsh);
+return (hsh);
 }
